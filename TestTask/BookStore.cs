d97a3486ae5d36fd6f26e06b00ce1f45a8b7082a
Дмитрий <@DESKTOP-IP3KOQF>
2021-04-17 12:12:19 +0300

    Добавьте файлы проекта.

---
+++ TestTask/BookStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestTask
+{
+    class BookStore
+    {
+        public BookStore()
+        {
+            using (StreamReader stream = new StreamReader("books.json"))
+            {
+                Books = JsonConvert.DeserializeObject(stream.ReadToEnd()) as ObservableCollection<Book>;
+            }
+        }
+        
+        public ObservableCollection<Book> Books { get; set; }
+
+        public void Sort()
+        {
+
+        }
+
+        public void Buy()
+        {
+
+        }
+
+    }
+}
